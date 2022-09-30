d5ab485366140ccc16445264d81dd5778d33e313
IraMonasteretska <44293548+IraMonasteretska@users.noreply.github.com>
2022-09-30 16:46:48 +0300

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -30,7 +30,7 @@ $(document).ready(function () {
                     slidesPerView: 2,
                     spaceBetween: 30
                 },
-                
+
             }
         });
     }
@@ -47,8 +47,139 @@ $(document).ready(function () {
     });
 
 
-   $('#household').change(function(){
+    $('#household').change(function () {
         $(this).parents('.liverowinpfield').next('.liverowinpfield').removeClass('hideinput');
-   });
+    });
+
+
+
+    //    steps
+    // next
+    $('.btnsfield a').not('.step1 .back').click(function (e) {
+        e.preventDefault();
+        $('.completprofile__step').hide();
+    });
+
+    $('.step1 .next').click(function () {
+        $('.step2').show();
+        $('.completprofile__steppoints').removeClass('pointsstep1');
+        $('.completprofile__steppoints').addClass('pointsstep2');
+    });
+    $('.step2 .next').click(function () {
+        $('.step3').show();
+        $('.completprofile__steppoints').removeClass('pointsstep2');
+        $('.completprofile__steppoints').addClass('pointsstep3');
+    });
+    $('.step3 .next').click(function () {
+        $('.step4').show();
+        $('.completprofile__steppoints').removeClass('pointsstep3');
+        $('.completprofile__steppoints').addClass('pointsstep4');
+    });
+    $('.step4 .next').click(function () {
+        $('.step5').show();
+        $('.completprofile__steppoints').removeClass('pointsstep4');
+        $('.completprofile__steppoints').addClass('pointsstep5');
+    });
+    $('.step5 .next').click(function () {
+        $('.step6').show();
+        $('.completprofile__steppoints').removeClass('pointsstep5');
+        $('.completprofile__steppoints').addClass('pointsstep6');
+    });
+    $('.step6 .next').click(function () {
+        $('.step7').show();
+        $('.completprofile__steppoints').removeClass('pointsstep6');
+        $('.completprofile__steppoints').addClass('pointsstep7');
+    });
+    $('.step7 .next').click(function () {
+        $('.step8').show();
+        $('.completprofile__steppoints').removeClass('pointsstep7');
+        $('.completprofile__steppoints').addClass('pointsstep8');
+    });
+    $('.step8 .finish').click(function () {
+        $('.completprofile').hide();
+        $('.loaderbox').removeClass('hide');
+    });
+    // back
+    $('.step8 .back').click(function () {
+        $('.step7').show();
+        $('.completprofile__steppoints').removeClass('pointsstep8');
+        $('.completprofile__steppoints').addClass('pointsstep7');
+    });
+    $('.step7 .back').click(function () {
+        $('.step6').show();
+        $('.completprofile__steppoints').removeClass('pointsstep7');
+        $('.completprofile__steppoints').addClass('pointsstep6');
+    });
+    $('.step6 .back').click(function () {
+        $('.step5').show();
+        $('.completprofile__steppoints').removeClass('pointsstep6');
+        $('.completprofile__steppoints').addClass('pointsstep5');
+    });
+    $('.step5 .back').click(function () {
+        $('.step4').show();
+        $('.completprofile__steppoints').removeClass('pointsstep5');
+        $('.completprofile__steppoints').addClass('pointsstep4');
+    });
+    $('.step4 .back').click(function () {
+        $('.step3').show();
+        $('.completprofile__steppoints').removeClass('pointsstep4');
+        $('.completprofile__steppoints').addClass('pointsstep3');
+    });
+    $('.step3 .back').click(function () {
+        $('.step2').show();
+        $('.completprofile__steppoints').removeClass('pointsstep3');
+        $('.completprofile__steppoints').addClass('pointsstep2');
+    });
+    $('.step2 .back').click(function () {
+        $('.step1').show();
+        $('.completprofile__steppoints').removeClass('pointsstep2');
+        $('.completprofile__steppoints').addClass('pointsstep1');
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    $('.step8 .finish').click(function () {
+        let counter = 0;
+        const maxCounter = 100;
+        const seconds = 5;
+        const interval = (seconds * 1000) / maxCounter
+        const loop = setInterval(function () {
+            document.getElementById('display').innerHTML = counter
+            if (counter === maxCounter) {
+                stopLoop()
+            }
+            counter++
+        }, interval);
+    
+        function stopLoop() {
+            clearInterval(loop)
+        }
+    });
+
+
+  
+
+
+
+
+
+
+
 
 });
